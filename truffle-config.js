ddcd085e65d2a757eef89b1046b26c1f3c29f818
akshaydevh <akshaydevklm@gmail.com>
2022-05-25 14:48:15 +0530

    bsc seed updated

--- truffle-config.js
+++ truffle-config.js
@@ -24,7 +24,7 @@ const mnemonic = process.env.MNEMONIC;
 const infuraRinkeby = `wss://:${process.env.INFURA_SECRET}@rinkeby.infura.io/ws/v3/${process.env.INFURA_ID}`;
 const mumbaiTestnetSeed = "https://matic-mumbai.chainstacklabs.com";
 const polygonMainnetSeed = "https://polygon-rpc.com/"
-const binanceMainnetSeed = "https://bsc-dataseed1.ninicoin.io/"
+const binanceMainnetSeed = "https://bsc-dataseed.binance.org/"
 const infura = `https://:${process.env.INFURA_SECRET}@mainnet.infura.io/v3/${process.env.INFURA_ID}`;
 const binanceTestnetSeed = "https://data-seed-prebsc-2-s3.binance.org:8545/";
 
